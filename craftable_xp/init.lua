589d7325d8bc1c24ea43f19d4d6baf974feeeb31
daninja123 <harbingerninja9@gmail.com>
2024-12-16 18:27:02 -0600

    Add files via upload

---
+++ craftable_xp/init.lua
@@ -0,0 +1,11 @@
+--shrimple :) *input 'i am just a chill guy meme'*
+
+core.register_craft({
+    type = "shaped"
+    output = "mcl_experience_bottle 16",
+    recipe = {
+        {"", "mcl_core_sugar" ""            },
+        {"mcl_core_lapis", "mcl_brewing_bottle_bg" "default_gold_ingot"   },
+        {"", "mcl_core_emerald", ""         },
+    }
+})
